93df32b0d1f6b0354c74b0da19eaeb60e23f533d
Jack Komaroff <jackkomaroff23@gmail.com>
2020-07-15 16:26:35 -0400

    testing swift github

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SandboxAppDev
  Files from Mobile App Development with Switft HM 2020-2021
+
+ Don't use without giving credit to me (Jack Komaroff). 
---
+++ test.swift
@@ -0,0 +1,20 @@
+func name(parameters) -> Type {
+    
+}
+if condition {
+    
+}
+
+if condition {
+    
+} else {
+    
+}
+
+for item in collection {
+    
+}
+
+this is a tester file
+
+NOT REAL CODE
